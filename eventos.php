dc213373f8227c71c1b7300683e94aa3278ce64b
Agustin Romero <agusromero0815@gmail.com>
2023-06-23 00:14:11 -0300

    refactoring

--- eventos.php
+++ eventos.php
@@ -2,41 +2,7 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!--
-    - primary meta tags
-  -->
-  <title>AGOC - Tu aliado gastronómico y estratégico</title>
-  <meta name="title" content="Grilli - Amazing & Delicious Food">
-  <meta name="description" content="This is a Restaurant html template made by codewithsadee">
-
-  <!--
-    - favicon
-  -->
-  <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml">
-
-  <!--
-    - google font link
-  -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&family=Forum&display=swap" rel="stylesheet">
-
-  <!--
-    - custom css link
-  -->
-  <link rel="stylesheet" href="./assets/css/style.css">
-
-  <!--
-    - preload images
-  -->
-  <link rel="preload" as="image" href="./assets/images/hero-slider-1.jpg">
-  <link rel="preload" as="image" href="./assets/images/hero-slider-2.jpg">
-  <link rel="preload" as="image" href="./assets/images/hero-slider-3.jpg">
-
+  <?php require_once './php/head.php' ?>
 </head>
 
 <body id="top">
@@ -267,12 +233,12 @@
             <p class="label-2 section-subtitle slider-reveal">Tecnología y gastronomía</p>
 
             <h1 class="display-1 hero-title slider-reveal">
-              ¡Descubrí  <br>
+              ¡Descubrí <br>
               AGOC!
             </h1>
 
             <p class="body-2 hero-text slider-reveal">
-               La plataforma líder en soluciones tecnológicas gastronómicas<br>
+              La plataforma líder en soluciones tecnológicas gastronómicas<br>
               que transformará por completo tu negocio.
             </p>
 
@@ -368,138 +334,7 @@
     - #FOOTER
   -->
 
-  <footer class="footer section has-bg-image text-center"
-    style="background-image: url('./assets/images/footer-bg.jpg')">
-    <div class="container">
-
-      <section id="contact">
-        <div class="footer-top grid-list">
-
-          <div class="footer-brand has-before has-after">
-
-            <a href="#" class="logo">
-              <img src="./assets/images/logo_blanco.png" width="160" height="50" loading="lazy" alt="AGOC">
-            </a>
-
-            <address class="body-4">
-              San Ramón, Alajuela, Cr
-            </address>
-
-            <a href="mailto:booking@grilli.com" class="body-4 contact-link">info@agoccr.com</a>
-
-            <a href="tel:+88123123456" class="body-4 contact-link">Programá una cita : +506 0000-0000</a>
-
-            <p class="body-4">
-              Horario : 09:00 am - 04:00 pm
-            </p>
-
-            <div class="wrapper">
-              <div class="separator"></div>
-              <div class="separator"></div>
-              <div class="separator"></div>
-            </div>
-
-            <p class="title-1">Recibí Noticias y Ofertas</p>
-
-            <p class="label-1">
-              Suscribíte ahora y obtené <span class="span">beneficios únicos.</span>
-            </p>
-
-            <form action="" class="input-wrapper">
-              <div class="icon-wrapper">
-                <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
-
-                <input type="email" name="email_address" placeholder="Tu correo" autocomplete="off" class="input-field">
-              </div>
-
-              <button type="submit" class="btn btn-secondary">
-                <span class="text text-1">Subscribirse</span>
-
-                <span class="text text-2" aria-hidden="true">Subscribirse</span>
-              </button>
-            </form>
-
-          </div>
-
-          <ul class="footer-list">
-
-            <li>
-              <a href="index.php#home" class="label-2 footer-link hover-underline">Inicio</a>
-            </li>
-
-            <li>
-              <a href="index.php#servicios" class="label-2 footer-link hover-underline">Servicios</a>
-            </li>
-
-            <li>
-              <a href="index.php#ruta" class="label-2 footer-link hover-underline">Ruta Gastronómica</a>
-            </li>
-
-            <li>
-              <a href="index.php#tips" class="label-2 footer-link hover-underline">tips</a>
-            </li>
-
-            <li>
-              <a href="index.php#productos" class="label-2 footer-link hover-underline">Productos</a>
-            </li>
-
-            <li>
-              <a href="index.php#eventos" class="label-2 footer-link hover-underline">Eventos</a>
-            </li>
-
-            <li>
-              <a href="index.php#paquetes" class="label-2 footer-link hover-underline">Paquetes</a>
-            </li>
-
-            <li>
-              <a href="index.php#about" class="label-2 footer-link hover-underline">Nosotros</a>
-            </li>
-
-            <li>
-              <a href="#contact" class="label-2 footer-link hover-underline">Contacto</a>
-            </li>
-
-          </ul>
-
-          <ul class="footer-list">
-
-            <li>
-              <a href="#" class="label-2 footer-link hover-underline">Facebook</a>
-            </li>
-
-            <li>
-              <a href="#" class="label-2 footer-link hover-underline">Instagram</a>
-            </li>
-
-            <li>
-              <a href="#" class="label-2 footer-link hover-underline">Twitter</a>
-            </li>
-
-            <li>
-              <a href="#" class="label-2 footer-link hover-underline">Youtube</a>
-            </li>
-
-            <li>
-              <a href="#" class="label-2 footer-link hover-underline">Google Map</a>
-            </li>
-
-          </ul>
-
-        </div>
-      </section>
-
-
-      <div class="footer-bottom">
-
-        <p class="copyright">
-          &copy; 2022 AGOC CR. Todos los derechos reservados | Base creada por: <a href="https://github.com/codewithsadee"
-            target="_blank" class="link">codewithsadee</a>
-        </p>
-
-      </div>
-
-    </div>
-  </footer>
+  <?php require_once './php/footer.php' ?>
 
 
 
